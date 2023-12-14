4394e2609b4b9d1bfc36f285c22e4a2273a607c0
d00m4ace <d00m4ace79@gmail.com>
2023-12-14 20:46:11 +0200

    Development phase 1

--- code/render/gui/text.c
+++ code/render/gui/text.c
@@ -4,11 +4,11 @@ GUI_ELEM* gui_text_(const char* txt, GUI_TEXT_STYLE_ENUM txt_style)
 
 	switch(txt_style)
 	{
-	case GUI_TEXT_STYLE_NORMAL: elm->style = GUI_STYLE_TEXT; break;
-	case GUI_TEXT_STYLE_SMALL: elm->style = GUI_STYLE_TEXT_SMALL; break;
-	case GUI_TEXT_STYLE_NORMAL_BORDER: elm->style = GUI_STYLE_TEXT_BORDER; break;
-	case GUI_TEXT_STYLE_SMALL_BORDER: elm->style = GUI_STYLE_TEXT_SMALL_BORDER; break;
-	default: elm->style = GUI_STYLE_TEXT; break;
+		case GUI_TEXT_STYLE_NORMAL: elm->style = GUI_STYLE_TEXT; break;
+		case GUI_TEXT_STYLE_SMALL: elm->style = GUI_STYLE_TEXT_SMALL; break;
+		case GUI_TEXT_STYLE_NORMAL_BORDER: elm->style = GUI_STYLE_TEXT_BORDER; break;
+		case GUI_TEXT_STYLE_SMALL_BORDER: elm->style = GUI_STYLE_TEXT_SMALL_BORDER; break;
+		default: elm->style = GUI_STYLE_TEXT; break;
 	}
 
 	elm->flags |= GUI_FLG_LOCKED;
@@ -24,20 +24,19 @@ GUI_ELEM* gui_text_word_wrap_(const char* txt, GUI_TEXT_STYLE_ENUM txt_style, in
 
 	switch(txt_style)
 	{
-	case GUI_TEXT_STYLE_NORMAL: elm->style = GUI_STYLE_TEXT; break;
-	case GUI_TEXT_STYLE_SMALL: elm->style = GUI_STYLE_TEXT_SMALL; break;
-	case GUI_TEXT_STYLE_NORMAL_BORDER: elm->style = GUI_STYLE_TEXT_BORDER; break;
-	case GUI_TEXT_STYLE_SMALL_BORDER: elm->style = GUI_STYLE_TEXT_SMALL_BORDER; break;
-	default: elm->style = GUI_STYLE_TEXT; break;
+		case GUI_TEXT_STYLE_NORMAL: elm->style = GUI_STYLE_TEXT; break;
+		case GUI_TEXT_STYLE_SMALL: elm->style = GUI_STYLE_TEXT_SMALL; break;
+		case GUI_TEXT_STYLE_NORMAL_BORDER: elm->style = GUI_STYLE_TEXT_BORDER; break;
+		case GUI_TEXT_STYLE_SMALL_BORDER: elm->style = GUI_STYLE_TEXT_SMALL_BORDER; break;
+		default: elm->style = GUI_STYLE_TEXT; break;
 	}
 
 	elm->flags |= GUI_FLG_LOCKED;
 	elm->flags |= GUI_FLG_WORD_WRAP;
 
-	elm->size = gui_helper_txt_calc_content_size_word_wrap(txt, gui_style_get(elm->style), elm->gui_state, (p32){ max_width, max_height});
+	elm->size = gui_helper_txt_calc_content_size_word_wrap(txt, gui_style_get(elm->style), elm->gui_state, (p32) { max_width, max_height });
 
 	elm->calc_size = gui_element_calc_size;
 
 	return elm;
 }
-
